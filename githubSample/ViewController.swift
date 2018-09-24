93253b5d662eebbe986fa5f585e883d7357daedc
ARTEAGA,KA KEVIN <rl106856@rlas-macair-20.rlas-116.org>
2018-09-24 10:46:03 -0500

    Initial Commit

---
+++ githubSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubSample
+//
+//  Created by ARTEAGA,KA KEVIN on 9/24/18.
+//  Copyright © 2018 ARTEAGA,KA KEVIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
